f4acde1bd8339fb93944fe6d1d2f09efdd71098b
ChrisOfNormandy <boobrick@hotmail.com>
2021-01-06 22:18:17 -0500

    Init release.

--- src/main/java/com/github/chrisofnormandy/conlib/item/Fuel.java
+++
@@ -1,9 +0,0 @@
-package com.github.chrisofnormandy.conlib.item;
-
-import net.minecraft.item.Item;
-
-public class Fuel extends Item {
-    public Fuel(Properties properties) {
-        super(properties);
-    }
-}
