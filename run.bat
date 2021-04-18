1bd8721972183bdaf530a5a5208d828f7aa5fd6c
ChrisOfNormandy <boobrick@hotmail.com>
2021-04-18 14:33:13 -0400

    idk what I'm doing...

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 mvn install:install-file ^
 -DgroupId=com.github.chrisofnormandy ^
 -DartifactId=conlib ^
--Dversion=1.0 ^
--Dfile=build/libs/conlib-1.0-deobf.jar ^
+-Dversion=1.1 ^
+-Dfile=build/libs/conlib-1.1-deobf.jar ^
 -Dpackaging=jar ^
 -DlocalRepositoryPath=. ^
 -DcreateChecksum=true ^
--- runRename.bat
+++ runRename.bat
@@ -1,11 +1,11 @@
-cd com/github/chrisofnormandy/conlib/1.0/
+cd com/github/chrisofnormandy/conlib/1.1/
 
-ren conlib-1.0.jar conlib-1.0-deobf.jar
-ren conlib-1.0.jar.md5 conlib-1.0-deobf.jar.md5
-ren conlib-1.0.jar.sha1 conlib-1.0-deobf.jar.sha1
-ren conlib-1.0.pom conlib-1.0-deobf.pom
-ren conlib-1.0.pom.md5 conlib-1.0-deobf.pom.md5
-ren conlib-1.0.pom.sha1 conlib-1.0-deobf.pom.sha1
+ren conlib-1.1.jar conlib-1.1-deobf.jar
+ren conlib-1.1.jar.md5 conlib-1.1-deobf.jar.md5
+ren conlib-1.1.jar.sha1 conlib-1.1-deobf.jar.sha1
+ren conlib-1.1.pom conlib-1.1-deobf.pom
+ren conlib-1.1.pom.md5 conlib-1.1-deobf.pom.md5
+ren conlib-1.1.pom.sha1 conlib-1.1-deobf.pom.sha1
 
 cd ..
 cd ..
